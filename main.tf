a3ccef49cdd32bee8f6ebd33f40e65f11fb9afcb
Gurpreet Singh <gurpreeteyebrow@gmail.com>
2023-11-28 18:42:59 +0530

    updated subnets_dev to a list

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
   # For prod, we have to create a dynamic map that can deploy managers and workers in different subnets or AZs
   # For dev, only 1 manager node and 2 worker nodes 
   # if list with 3 subnet IDs come, slice it get first 2 subnetIDs to deploy our worker nodes to, else keep it same
-  subnets_dev = length(module.module-vpc-3tier.private_subnet_ids) == 3 ? slice(module.module-vpc-3tier.private_subnet_id, 0, 1) : module.module-vpc-3tier.private_subnet_ids
+  subnets_dev = slice(module.module-vpc-3tier.private_subnet_id, 0, 1)
 }
 
 # first we will create vpc setup with the help of our custom module
