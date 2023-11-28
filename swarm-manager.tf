6f93fbdd0ab22c137864f0c8a207339fdf886070
Gurpreet Singh <gurpreeteyebrow@gmail.com>
2023-11-28 18:55:12 +0530

    added security group id to ec2 nodes

--- swarm-manager.tf
+++ swarm-manager.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "swarm-manager" {
   instance_type   = var.ec2_instance_type
   count           = 1
   subnet_id       = module.module-vpc-3tier.private_subnet_ids[0]
-  security_groups = [aws_security_group.swarm-sg.name]
+  security_groups = [aws_security_group.swarm-sg.id]
 
   tags = {
     Name = "${var.app}-swarm-manager-${terraform.workspace}"
@@ -21,7 +21,7 @@ resource "aws_instance" "bastion-host" {
   instance_type = var.ec2_instance_type
   subnet_id     = module.module-vpc-3tier.public_subnet_ids[0]
 
-  security_groups = [aws_security_group.bastion-sg.name]
+  security_groups = [aws_security_group.bastion-sg.id]
 
   tags = {
     Name = "${var.app}-bastion-host-${terraform.workspace}"
--- swarm-workers.tf
+++ swarm-workers.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "worker" {
   ami             = data.aws_ami.ubuntu.id
   instance_type   = var.ec2_instance_type
   subnet_id       = local.subnets_dev[count.index]
-  security_groups = [aws_security_group.swarm-sg.name]
+  security_groups = [aws_security_group.swarm-sg.id]
   tags = {
     Name = "${var.app}-swarm-worker-${count.index + 1}-${terraform.workspace}"
     env  = local.env
