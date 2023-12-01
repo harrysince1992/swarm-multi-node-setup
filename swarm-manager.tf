62d2abc94776a15ff9389bfcc228fcc87da39557
Gurpreet Singh <gurpreeteyebrow@gmail.com>
2023-12-01 19:07:43 +0530

    added sleep for 2 mins

--- swarm-manager.tf
+++ swarm-manager.tf
@@ -51,6 +51,7 @@ resource "aws_instance" "bastion-host" {
     inline = [
       "chmod 400 /home/ubuntu/swarm-key.pem",
       "export ANSIBLE_HOST_KEY_CHECKING=False",
+      "sleep 120",
       "/usr/bin/ansible-playbook -i /home/ubuntu/${local_file.inventory.filename} --private-key /home/ubuntu/swarm-key.pem /home/ubuntu/${local_file.swarm-playbook.filename}"
     ]
   }
